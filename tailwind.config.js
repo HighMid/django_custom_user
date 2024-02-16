baf06a2031d1920ffe3f3c726d966815a3dbc5ec
HighMid <xstar671@gmail.com>
2024-02-15 21:52:46 -0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/*.html',
+    './static/js/**/*.js',
+    // Add any other paths that contain Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
